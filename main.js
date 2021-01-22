dfdba336a57adc3549f7cd927b20f56bcdc95efe
Ryan Souza <ryanssouza00@gmail.com>
2021-01-22 19:27:11 -0300

    🎉 adding theme changer

--- main.js
+++ main.js
@@ -8,3 +8,31 @@ const Modal = {
         modal.classList.remove('active')
     }
 }
+
+
+const Theme = {
+    dark() {
+        const html = document.querySelector('html')
+        const image = document.querySelector('.change-theme img')
+        image.src = 'assets/sun.svg'
+        html.setAttribute('data-theme', 'dark')
+    },
+    light() {
+        const html = document.querySelector('html')
+        const image = document.querySelector('.change-theme img')
+        image.src = 'assets/moon.svg'
+        html.setAttribute('data-theme', 'light')
+    },
+
+    change() {
+        const html = document.querySelector('html')
+
+        const theme = html.getAttribute('data-theme');
+
+        if(theme == 'dark') {
+            Theme.light()
+        }else {
+            Theme.dark()
+        }
+    },
+}
